3068d74e4c6c0d27c981903af281468b880358ea
Anna Kalinowska <mirska.anna@gmail.com>
2022-11-16 19:49:25 +0100

    fixes

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,4 +1,15 @@
 export const convertPLNToUSD = (PLN) => {
+  if (typeof PLN === 'object' || typeof PLN === 'function') {
+    return 'Error';
+  }
+  if (typeof PLN === 'string' || typeof PLN === 'undefined') {
+    //return parseInt(formatter.format(PLNtoUSD).replace(/\u00a0/g, ' '));
+    return NaN;
+  }
+  if (PLN < 0) {
+    return '$0.00';
+  }
+
   const PLNtoUSD = PLN / 3.5;
 
   const formatter = new Intl.NumberFormat('en-US', {
@@ -6,22 +17,9 @@ export const convertPLNToUSD = (PLN) => {
     currency: 'USD',
   });
 
-  const F = formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+  // const F = formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 
-  console.log(PLN, F, typeof PLN);
-
-  if (typeof PLN === 'object' || typeof PLN === 'function') {
-    return 'Error';
-  }
-  if (
-    (Number.isInteger(PLN) === false && typeof PLN === 'string') ||
-    typeof PLN === 'undefined'
-  ) {
-    return parseInt(formatter.format(PLNtoUSD).replace(/\u00a0/g, ' '));
-  }
-  if (PLN < 0) {
-    return '$0.00';
-  }
+  // console.log(PLN, F, typeof PLN);
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 };
